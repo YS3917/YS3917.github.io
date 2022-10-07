d4bce0137fb9d03dc6f3e9c00b8a99e470706738
yongseok <tjdydtjr85@gmail.com>
2022-10-07 10:29:09 +0900

    포트폴리오 탭메뉴 기능

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 window.onload = function () {
-  AOS.init();
+  //   AOS.init();
+
+    let vueboxcheck = $(".vue-box");
+    let clonboxcheck = $(".clon-box");
+    let studyClone = $(".study-clone");
+    let studyVue = $(".study-vue");
+
+    studyVue.click(function (e) {
+        e.preventDefault();
+        vueboxcheck.addClass("portfolio-wrap-box-active");
+        clonboxcheck.removeClass("portfolio-wrap-box-active");
+        studyVue.addClass("portfolio-menu-active");
+        studyClone.removeClass("portfolio-menu-active");
+    });
+
+    studyClone.click(function (e) {
+        e.preventDefault();
+        clonboxcheck.addClass("portfolio-wrap-box-active");
+        vueboxcheck.removeClass("portfolio-wrap-box-active");
+        studyClone.addClass("portfolio-menu-active");
+        studyVue.removeClass("portfolio-menu-active");
+    });
 };
